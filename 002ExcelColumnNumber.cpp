c9218bbc9ce631c918050c0c394dfd0a5eaa57cd
Tanveer Ahmad Khan <tnvr000@gmail.com>
2023-06-23 18:06:25 +0530

    easy questions batch 1

---
+++ 002ExcelColumnNumber.cpp
@@ -0,0 +1,13 @@
+/*
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number
+
+for example
+ * A  -> 1
+ * B  -> 2
+ * C  -> 3
+ * .....
+ * .....
+ * Z  -> 26
+ * AA -> 27
+ * AB -> 28
+*/
---
+++ 005SPSDI.cpp
@@ -0,0 +1,11 @@
+/*
+Given an integer number n, return the difference between the product of its digits and the sum of its digits. 
+
+example
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+*/
