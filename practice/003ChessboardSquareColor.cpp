98d96e551be7a097a499b3238daff80fef1ac7f7
Goushia Parween <goushiaparween111@gmail.com>
2023-06-27 18:04:15 +0530

    chess board

--- practice/003ChessboardSquareColor.cpp
+++ practice/003ChessboardSquareColor.cpp
@@ -22,3 +22,23 @@ You are given coordinates, a string that represents the coordinates of a square 
 
 Return true if the square is white, and false if the square is black.
 */
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+  char add[2];
+  int sum;
+
+  cout<<"Enter : ";
+  scanf("%s", add);
+
+  sum = (int)add[0] + (int)add[1];
+  if(sum % 2 == 0){
+    cout<<"black";
+  }
+  else
+    cout<<"white";
+}
