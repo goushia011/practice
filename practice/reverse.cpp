5b165ffb4d8eea087dae43f009805014a6573b75
Goushia Parween <goushiaparween111@gmail.com>
2023-07-02 16:54:39 +0530

    reverse

--- practice/reverse.cpp
+++ practice/reverse.cpp
@@ -5,10 +5,19 @@ using namespace std;
 int main(){
     
     string str;
-    cout<<"Enter any string\t";
-    cin>>str;
-    int n=str.length();
-    for(int i=n-1;i>=0;i--){
-        cout<<str[i];
+
+    cout<<"Enter any string : ";
+    getline(cin, str);
+
+    int size =str.length();
+
+    for(int i = 0; i < size / 2; i++)
+    {
+        int temp;
+        temp = str[i];
+        str[i] = str[(size - 1) -i];
+        str[(size - 1) -i] = temp;
+        cout<<str<<" ";
     }
+    cout<<endl<<str;
 }
