8a2c83c44acc40ef64a98da6fb1883420490510d
Goushia Parween <goushiaparween111@gmail.com>
2023-07-05 17:11:06 +0530

    anagram

--- practice/011Nangram.cpp
+++ practice/011Nangram.cpp
@@ -13,5 +13,43 @@ Output: true
 Example 2:
 Input: s = "rat", t = "car"
 Output: false
-
 */
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+    int size1, size2, flag = 1;
+
+    cout<<"enter 1st string : ";
+    getline(cin, str1);
+    cout<<endl;
+    cout<<"enter 2nd string : ";
+    getline(cin, str2);
+    size1 = str1.length();
+    size2 = str2.length();
+     
+     for(int i = 0; i < size1; i++)
+     {
+        int index = str2.find(str1[i]);
+        if(index == -1)
+        {
+            flag = 0;
+            break;
+        } 
+        else 
+        {
+            str2[index] = '_';
+        }
+     }
+     if(flag == 1)
+     {
+        cout<<"anagram";
+     }
+     else
+     {
+        cout<<"not an anagram";
+     }
+}
