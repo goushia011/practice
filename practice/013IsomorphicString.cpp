c63f990a466484d3fadfd4cf6c71e71c27248788
Goushia Parween <goushiaparween111@gmail.com>
2023-07-08 17:32:06 +0530

    isomorphic

--- practice/013IsomorphicString.cpp
+++ practice/013IsomorphicString.cpp
@@ -22,3 +22,57 @@ Input: s = "paper", t = "title"
 Output: true
 
 */
+
+#include<iostream>
+#include<string>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+    map<char, char> mymap;
+    map<char, char>::iterator itr;
+    string s, t;
+    int flag = 1;
+
+    cout<<"enter s: ";
+    getline(cin, s);
+    cout<<"enter t: ";
+    getline(cin, t);
+
+    if(s.length() == t.length())
+    {
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(mymap[t[i]] == '\0')
+            {
+                mymap[t[i]] = s[i];
+            }
+            else
+            {
+                if(mymap[t[i]] != s[i])
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+    }
+    else{
+        flag = 0;
+    }
+         
+    for(itr = mymap.begin(); itr != mymap.end(); itr++)
+    {
+        cout<<itr->first<<" "<<itr->second<<endl;
+    }
+    
+    if(flag == 0)
+    {
+        cout<<"not isomorphic";
+    }
+    else{
+        cout<<"isomorphic";
+    }
+}
