e71e5d01ef0d8ee81035d97be9a5372deac8846e
Goushia Parween <goushiaparween111@gmail.com>
2023-07-09 16:59:57 +0530

    uniquecharacter

--- practice/014UniqueCharacter.cpp
+++ practice/014UniqueCharacter.cpp
@@ -16,3 +16,48 @@ Input: s = "aabb"
 Output: -1
 
 */
+#include<iostream>
+#include<string>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    map<char, int> mymap;
+
+    cout<<"enter string: ";
+    getline(cin, s);
+
+    for(int i = 0; i < s.length(); i++)
+        mymap[s[i]] += 1;
+
+    int i, j, in = -1;
+    for(i = 0; i < s.length(); i++)
+    {
+        for(j = 0; j < s.length(); j++)
+        {
+            if(i == j)
+                continue;
+
+            if(s[i] == s[j])
+            {
+                break;
+            }
+        }
+        if(j == s.length())
+        {
+            in = i;
+            break;
+        }
+    }
+
+    if(in == -1)
+    {
+        cout<<"no unique character";
+    }
+    else
+        cout<<in<<" "<<s[in];
+
+}
