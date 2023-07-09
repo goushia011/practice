5b85ba9d51d832d02194371d5a3136f4b0fed0b1
Goushia Parween <goushiaparween111@gmail.com>
2023-07-09 12:37:00 +0530

    segments

--- practice/015Segment.cpp
+++ practice/015Segment.cpp
@@ -15,3 +15,24 @@ Input: s = "Hello"
 Output: 1
 
 */
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int count = 1;
+
+    cout<<"enter string ";
+    getline(cin, s);
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == ' ')
+        {
+            count++;
+        }
+    }
+  cout<<count;
+}
