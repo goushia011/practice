ef3acec0c0231f2ffcfd9f3bf943b2de682f067a
Goushia Parween <goushiaparween111@gmail.com>
2023-07-17 17:13:40 +0530

    pares integer

--- practice/027ParseInteger.cpp
+++ practice/027ParseInteger.cpp
@@ -13,15 +13,67 @@ Output: Error
 
 using namespace std;
 
+int strintTointeger(char s)
+{
+    switch(s)
+    {
+        case '1':
+            return 1;
+            break;
+        case '2':
+            return 2;
+            break;
+        case '3':
+            return 3;
+            break;
+        case '4':
+            return 4;
+            break;
+        case '5':
+            return 5;
+            break;
+        case '6':
+            return 6;
+            break;
+        case '7':
+            return 7;
+            break;
+        case '8':
+            return 8;
+            break;
+        case '9':
+            return 9;
+            break;
+        case '0':
+            return 0;
+            break;
+            
+    }
+
+}
+
 int main()
 {
     string s, n;
+    int  divider = 0;
+    float result = 0;
 
     cout<<"enter string: ";
     cin>>s;
 
     for(int i = 0; i < s.length(); i++)
     {
-        n = ints[i];
+        if(s[i] == '.')
+        {
+            divider = 1;
+            continue;
+        }
+       result = ((result * 10) + strintTointeger(s[i]));
+       divider = divider * 10;
     }
+    if(divider != 0)
+    {
+        result = result / divider;
+    }
+    cout<<result;
 }
