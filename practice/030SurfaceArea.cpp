a85f2b86e0f6e19a5a63b3b97d6dd18c02db6771
Goushia Parween <goushiaparween111@gmail.com>
2023-07-21 16:39:19 +0530

    surfacearea

--- practice/030SurfaceArea.cpp
+++ practice/030SurfaceArea.cpp
@@ -42,11 +42,107 @@ example 3
 Input: grid = [[2,2,2],[2,1,2],[2,2,2]]
 Output: 46
 */
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
+int layerArea(int grid[20][20], int row, int col)
+{
+    int area = 0, lateralArea;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if(grid[i][j] > 0)
+            {
+            lateralArea = 4;
+            if (j > 0)
+            {
+                if (grid[i][j - 1] > 0)
+                {
+                    lateralArea--;
+                }
+            }
+            if (j < col - 1)
+            {
+                if (grid[i][j + 1] > 0)
+                {
+                    lateralArea--;
+                }
+            }
+            if (i > 0)
+            {
+                if (grid[i - 1][j] > 0)
+                {
+                    lateralArea--;
+                }
+            }
+            if (i < row - 1)
+            {
+                if (grid[i + 1][j] > 0)
+                {
+                    lateralArea--;
+                }
+            }
+            }
+            else{
+                lateralArea = 0;
+            }
+            cout << lateralArea << " ";
+            area += lateralArea;
+        }
+        cout << endl;
+    }
+
+    return area;
+}
+
+int decrementGrid(int grid[20][20], int row, int col)
+{
+    int sum = 0;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (grid[i][j] > 0)
+                grid[i][j]--;
+            sum += grid[i][j];
+        }
+    }
+    return sum;
+}
+
 int main()
 {
-    
+    int grid[20][20], row, col, area = 0, lateralArea;
+
+    cout << "enter row: ";
+    cin >> row;
+    cout << "enter col: ";
+    cin >> col;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> grid[i][j];
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (grid[i][j] > 0)
+                area += 2;
+        }
+    }
+    do
+    {
+        int la = layerArea(grid, row, col);
+        cout<<"la: "<<la<<" ";
+        area += layerArea(grid, row, col);
+    } while (decrementGrid(grid, row, col));
+
+    cout << area;
 }
